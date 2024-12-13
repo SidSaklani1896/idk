cdddc06b25c9f71a06636a4e634eb4ee218ea398
Siddharth Saklani <91240775+SidSaklani1896@users.noreply.github.com>
2024-12-13 18:07:36 +0530

    Update Story.jsx

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -49,12 +49,12 @@ const FloatingImage = () => {
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          the multiversal ip world
+          Growth and expansion
         </p>
 
         <div className="relative size-full">
           <AnimatedTitle
-            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            title="Creating Unstoppable Momentum"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 
@@ -106,11 +106,8 @@ const FloatingImage = () => {
         <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
           <div className="flex h-full w-fit flex-col items-center md:items-start">
             <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-              Where realms converge, lies Zentry and the boundless pillar.
-              Discover its secrets and shape your fate amidst infinite
-              opportunities.
+              Boulevard combines scalable infrastructure, advanced mechanics, and constant innovation to deliver a groundbreaking, ever-evolving gaming experience for a growing global community.
             </p>
-
             <Button
               id="realm-btn"
               title="discover prologue"
